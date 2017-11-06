c28a6630a7bc53e38b8a206fdbf3f86320af9460
Paul <paul.osbaldo@gmail.com>
2017-11-05 22:49:05 -0600

    se modifico texto de archivo swagger

--- src/main/java/com/redhat/developers/msa/ola/SwaggerConfiguration.java
+++ src/main/java/com/redhat/developers/msa/ola/SwaggerConfiguration.java
@@ -26,7 +26,7 @@ public class SwaggerConfiguration {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("Microservicio de API DEMO BANK")
+            .title("Microservicio de API DEMO BANK Seguridad")
             .description("Consultar operaciones al API del Banco Demo Seguridad")
 //            .contact(new Contact("AN Global", "",/*"http://developers.redhat.com"*/ "paul.montoya@agsnasoft.com"))
 //            .license("Apache License Version 2.0")
